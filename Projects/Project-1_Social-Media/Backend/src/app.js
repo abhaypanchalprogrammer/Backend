3c4b6f1ffd2aa2e1599ac5049c04fc2617687053
Abhay Panchal <abhaypanchal2525@gmail.com>
2026-03-01 12:23:59 +0530

    Update app.js

--- Projects/Project-1_Social-Media/Backend/src/app.js
+++ Projects/Project-1_Social-Media/Backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 /* ✅ DEFINE CORS OPTIONS ONCE */
 const corsOptions = {
-  origin: "https://app-socialmedia-app.vercel.app/",
+  origin: "https://app-socialmedia-app.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
